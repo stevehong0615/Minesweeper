df7dc7f7b8a5bc4a07aaaa496b34f03fc918f7a4
steve_hong <stevehong0615@gmail.com>
2016-08-23 09:59:33 +0000

    加入function

--- verification.php
+++ verification.php
@@ -2,19 +2,18 @@
 header("Content-Type:text/html; charset=utf-8");
 
 $map = $_GET['map'];
+
 $mapNoN = str_replace('N','',$map);
 $checkString = strlen($mapNoN);
 $mapNoA = str_split($mapNoN);
 $mapNoB = array_chunk($mapNoA, 10);
+
 $mapA = str_split($map);
 $stringCount = array_count_values($mapA);
-$standard = "/^([0-8M-N]+)$/";
-$checkNs = 1;
-$showRight = 0;
-$showLeft = 0;
 
 // 判斷炸彈周圍數字顯示是否正確
-if ($checkString == 100) {
+function bombNumber($mapNoB)
+{
     $check = true;
     for ($a = 0; $a < 10; $a++) {
         for ($b = 0; $b < 10; $b++) {
@@ -45,134 +44,105 @@ if ($checkString == 100) {
                     $mineCount++;
                 }
                 if ($mapNoB[$a][$b] != $mineCount){
-                    if($showLeft == 0) {
-                        echo "不符合，因為";
-                        $showLeft = 1;
-                    }
-                    echo "[" . $a . " , " . $b . "]" . " 判斷錯誤，應該為" . $mineCount;
+                    $messageReturn[$i] = "數字對應有錯" . "[" . $a . " , " . $b . "]" . " 判斷錯誤，應該為" . $mineCount . "---";
+                    $i++;
                     $check = false;
                 }
             }
         }
     }
-}
-
-// 字串符合的判斷
-if (mb_strlen($map) == 109) {
-    if ($stringCount['M'] < 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，炸彈數量小於40";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，炸彈數量小於40，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，炸彈數量小於40，N大於9";
-        }
-    }
-    if ($stringCount['M'] > 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，炸彈數量大於40";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，炸彈數量大於40，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，炸彈數量大於40，N大於9";
-        }
+    if ($messageReturn == null) {
+        return "yes";
+    } else {
+        return $messageReturn;
     }
 }
 
-// 不符合，字串長度小於109的判斷
-if (mb_strlen($map) < 109) {
+// 字串長度判斷
+function stringLen($map)
+{
+    if (mb_strlen($map) == 109) {
+        return "yes";
+    }
+    if (mb_strlen($map) < 109) {
+        return "字串長度小於109";
+    }
+    if (mb_strlen($map) > 109) {
+        return "字串長度大於109";
+    }
+}
+
+// 炸彈數量判斷
+function boNumber($stringCount)
+{
     if ($stringCount['M'] == 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，因為字串長度小於109";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，因為字串長度小於109，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，因為字串長度小於109，N大於9";
-        }
+        return "yes";
     }
-    if ($stringCount['M'] < 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，因為字串長度小於109，炸彈數量小於40";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，因為字串長度小於109，炸彈數量小於40，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，因為字串長度小於109，炸彈數量小於40，N大於9";
-        }
-    }
-    if ($stringCount['M'] > 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，因為字串長度小於109，炸彈數量大於40";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，因為字串長度小於109，炸彈數量大於40，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，因為字串長度小於109，炸彈數量大於40，N大於9";
-        }
+    if ($stringCount['M'] != 40) {
+        return "地雷數量有錯，有" . $stringCount['M'] . "顆地雷";
     }
 }
 
-// 不符合，字串長度大於109的判斷
-if (mb_strlen($map) > 109) {
-    if ($stringCount['M'] == 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，因為字串長度大於109";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，因為字串長度大於109，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，因為字串長度大於109，N大於9";
-        }
+// N數量判斷
+function nNumber($stringCount)
+{
+    if ($stringCount['N'] == 9) {
+        return "yes";
     }
-    if ($stringCount['M'] < 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，因為字串長度大於109，炸彈數量小於40";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，因為字串長度大於109，炸彈數量小於40，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，因為字串長度大於109，炸彈數量小於40，N大於9";
-        }
-    }
-    if ($stringCount['M'] > 40) {
-        if ($stringCount['N'] == 9) {
-            echo "不符合，因為字串長度大於109，炸彈數量大於40";
-        }
-        if ($stringCount['N'] < 9) {
-            echo "不符合，因為字串長度大於109，炸彈數量大於40，N小於9";
-        }
-        if ($stringCount['N'] > 9) {
-            echo "不符合，因為字串長度大於109，炸彈數量大於40，N大於9";
-        }
+    if ($stringCount['N'] != 9) {
+        return "N數量有錯，有" . $stringCount['N'] . "個N";
     }
 }
 
-// 符合
-if (mb_strlen($map) == 109 && $stringCount['M'] == 40 && $stringCount['N'] == 9 && $checkString == 100 && $check == true) {
+function stringCheck($mapA)
+{
+    $standard = "/^([0-8M-N]+)$/";
+    $checkNs = 1;
+    $j = 0;
+    foreach ($mapA as $value) {
+        if(!preg_match($standard, $value, $result)) {
+            if ($value == " ") {
+                $value = "%";
+            }
+            $message[$j] = "第" . $checkNs . "個字元為" . $value . "不符合規定---";
+            $j++;
+        }
+        $checkNs++;
+    }
+    if ($message == null) {
+        return "yes";
+    } else {
+        return $message;
+    }
+}
+
+$a = bombNumber($mapNoB, $showLeft);
+$b = stringLen($map);
+$c = boNumber($stringCount);
+$d = nNumber($stringCount);
+$e = stringCheck($mapA, $showRight, $checkNs);
+
+if ($a == "yes" && $b == "yes" && $c == "yes" && $d == "yes" && $e == "yes") {
     echo "符合";
-}
-
-foreach ($mapA as $value) {
-    if(!preg_match($standard, $value, $result)) {
-        if($showRight == 0) {
-            echo "不符合，因為";
-            $showRight = 1;
+} else {
+    echo "不符合，因為";
+    if ($a != "yes") {
+        foreach ($a as $value) {
+            echo $value;
         }
-        if ($value == " ") {
-            $value = "%";
-        }
-        echo "第" . $checkNs . "個字元為" . $value . "不符合規定";
     }
-$checkNs++;
+    if ($b != "yes") {
+        echo $b;
+    }
+    if ($c != "yes") {
+        echo $c;
+    }
+    if ($d != "yes") {
+        echo $d;
+    }
+    if ($e != "yes") {
+        foreach ($e as $value) {
+            echo $value;
+        }
+    }
 }
-
